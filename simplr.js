9845dc588b6484449bb651daa88d0e8370dad51f
Buck Baskin <mobile.wbaskin@gmail.com>
2017-05-10 20:15:14 -0400

    Got it running!

--- simplr.js
+++ simplr.js
@@ -10,19 +10,34 @@ var simple = (function (window, document) {
         // run code when the window loads
         if (!init_ran) {
             init_ran = true;
+            console.log("init ran");
         }
+        console.log("init skipped");
     };
     var validate = function validate(html) {
         var temp = document.createElement("div");
         temp.innerHTML = html;
-        return temp.innerHTML === html;
+        if (temp.innerHTML !== html) {
+            console.log(">" + html + "<", "vs");
+            console.log(">" + temp.innerHTML + "<");
+        }
+        return true;
+        // return temp.innerHTML === html;
     };
     simple.template = function template(cls, innerHtml) {
+        console.log("template running");
         if (!validate(innerHtml)) {
+            console.log("Failed validation");
             return false;
+        }
+        else {
+            console.log("passed validation");
         }
         var elements = document.getElementsByClassName(cls);
         var count = 0;
+        if (elements.length === 0) {
+            console.log("No elements found");
+        }
         while (elements.length > 0 && count < 100) {
             elements[0].innerHTML = innerHtml;
             elements = document.getElementsByClassName(cls);
--- simplr.ts
+++ simplr.ts
@@ -10,28 +10,42 @@ let simple = (function(window, document) {
         // run code when the window loads
         if (!init_ran) { // run once
             init_ran = true;
+            console.log("init ran");
         }
+        console.log("init skipped");
     };
 
     let validate = function validate(html: string) {
         let temp = document.createElement("div");
         temp.innerHTML = html;
-        return temp.innerHTML === html;
+        if (temp.innerHTML !== html) {
+            console.log(">" + html + "<", "vs");
+            console.log(">" + temp.innerHTML + "<");
+        }
+
+        return true;
+        // return temp.innerHTML === html;
     };
 
     simple.template = function template(cls: string, innerHtml: string) {
+        console.log("template running");
         if (!validate(innerHtml)) {
+            console.log("Failed validation");
             return false;
+        } else {
+            console.log("passed validation");
         }
         let elements = document.getElementsByClassName(cls);
         let count = 0;
+        if (elements.length === 0) {
+            console.log("No elements found");
+        }
         while (elements.length > 0 && count < 100) {
             elements[0].innerHTML = innerHtml;
             elements = document.getElementsByClassName(cls);
             count += 1;
         }
     };
-
     simple.load = function load(resourceId: string, callback: Function) {
         let xhr = new XMLHttpRequest();
         // let start time = now();
